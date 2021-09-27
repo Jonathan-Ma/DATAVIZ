71c85bb7d095256af15733fa05fb27977fcf8a60
Jonathan Ma <jma66@hawaii.edu>
2021-09-27 01:04:18 -1000

    scratch3 is now main

--- scratch.py
+++ scratch.py
@@ -15,8 +15,8 @@ app.layout = html.Div(
                 [
                     dcc.Dropdown(
                         id="dropdown",
-                        options=[{'label': 'Attack', 'value': 'atk'}, {'label': 'Death', 'value': 'dth'}],
-                        value=[],
+                        options=[{'label': 'About', 'value': 'abt'},{'label': 'Attack', 'value': 'atk'}, {'label': 'Death', 'value': 'dth'}],
+                        value=['abt'],
                         multi=True,
                     )
                 ]
@@ -24,6 +24,8 @@ app.layout = html.Div(
         ),
         html.Div(id="graph-container"),
         html.Div(id="graph-container2"),
+        html.Div(id="graph-container3"),
+
 
     ]
 )
@@ -39,7 +41,7 @@ def update_graph_scatter(value):
     if 'atk' in value:
         fig = px.scatter(df, x="sepal_width", y="sepal_length")
         return dcc.Graph(figure=fig)
-    return html.Div()
+    #return html.Div()
 
 
 @app.callback(
@@ -52,20 +54,20 @@ def update_graph_scatter2(value):
     if 'dth' in value:
         fig = px.scatter_3d(df, x="sepal_width", y="sepal_length", z='petal_width')
         return dcc.Graph(figure=fig)
-    return html.Div()
+    #return html.Div()
 
-
-# @app.callback(
-#     dash.dependencies.Output("graph-container", "children"),
-#     [dash.dependencies.Input("dropdown", "value")
-#      ],
-# )
-# def update_graph_scatter(value):
-#     if value:
-#         # Change the line below to dynamically create the figure based on value
-#         fig = px.scatter_3d(df, x="sepal_width", y="sepal_length", z='petal_width')
-#         return dcc.Graph(id="graph-container2", figure=fig)
-#     return html.Div()
+@app.callback(
+    dash.dependencies.Output("graph-container3", "children"),
+    [dash.dependencies.Input("dropdown", "value")
+     ],
+)
+def update_about(value):
+    if 'abt' in value:
+        return dcc.Markdown('''
+        # About
+        
+        ''')
+    # return html.Div()
 
 
 if __name__ == '__main__':
---
+++ scratch3.py
@@ -0,0 +1,87 @@
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+sidebar = html.Div(
+    [
+        html.H2("Attack on Cholera", className="display-4"),
+        html.A(
+            "Jonathan Ma", href="https://jonathan-ma.github.io/"
+        ),
+        html.Hr(),
+
+        dbc.Nav(
+            [
+                dbc.NavLink("About", href="/abt", active="exact"),
+                dbc.NavLink("Attack", href="/page-1", active="exact"),
+                dbc.NavLink("Page 2", href="/page-2", active="exact"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+content = html.Div(id="page-content", style=CONTENT_STYLE)
+
+app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
+
+
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname == "/abt":
+        return dcc.Markdown('''
+                # The Broad Street Cholera Outbreak
+                ### 
+                
+                
+                ##### Data
+                
+                
+                
+                ##### Libraries
+                
+                This program is written using Ploty's Dash framework
+        ''')
+
+    elif pathname == "/atk":
+        return html.P("This is the content of page 1. Yay!")
+
+    elif pathname == "/page-2":
+        return html.P("Oh cool, this is page 2!")
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
+    )
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
